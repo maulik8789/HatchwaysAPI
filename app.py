74b149825e8dd09630362388b9db2dbedee8baf5
maulikvaghela <maulik.vagh@gmail.com>
2022-02-20 23:44:09 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from textwrap import indent
+from flask import Flask, render_template, request, flash
+import json
+import requests
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '12345'
+
+#home page shows all posts..............................
+@app.route('/')
+def home():
+    url = "https://api.hatchways.io/assessment/blog/posts?tag="
+    r = requests.get(url)
+    data = json.loads(r.text)
+    return render_template('home.html', data = data)
+
+#showing posts related to tag/tags......................
+@app.route('/showposts', methods = ['POST'])
+def rec_names():
+    s = str(request.form.get('tag'))
+    if s == "" :
+        flash("Tags parameter is required")
+        return render_template('home.html')
+    s= s.replace("," , "&") 
+    url = f"https://api.hatchways.io/assessment/blog/posts?tag={s}"
+    r = requests.get(url)
+    data = json.loads(r.text)
+    return render_template('list.html', data = data)            
